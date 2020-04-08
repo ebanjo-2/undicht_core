962340e7f186466f448ad356b940e4452b8150b9
ebanjo-2 <eike.thed@gmail.com>
2020-04-08 13:19:00 +0200

    made the core projects includable into a solution by using dofile undicht_core/premake4_include_core_projects.lua in the premake4.lua file

--- premake4.lua
+++ premake4.lua
@@ -6,36 +6,8 @@ solution "undicht_core"
     buildoptions "-std=c++11"
     configurations { "debug - windows","debug - linux", "release - windows", "release - linux" }
 
+    -- basically include
+    dofile "premake4_include_core_projects.lua"
 
 
--- the project for the classes used to load the other core libraries
-
-project "core"
-    kind "SharedLib"
-
-	files { "include/**.*", "development_log.txt" }
-
-	includedirs { "include" }
-	
-	configuration "debug - linux"
-		links { "dl" }
-		flags { "Symbols" }
-		
-	configuration "release - linux"
-		links { "dl" }
-		flags { "OptimizeSpeed", "OptimizeSize" }
-		
-	configuration "debug - windows"
-		flags { "Symbols" }
-
-	configuration "release - windows"
-		flags { "OptimizeSpeed", "OptimizeSize" }
-
-
-
--- the core shared libs
-include "implementation/graphics/opengl_33"
-include "implementation/window/glfw"
-include "implementation/audio/openal"
-include "implementation/file/fstream"
     
