77c40a5ac092a60e8a9de024e155d42315005a7e
ebanjo-2 <eike.thed@gmail.com>
2020-10-03 14:40:49 +0200

    added submodule EGL-Registry for the khr_platform header

--- implementation/window/glfw/premake4.lua
+++ implementation/window/glfw/premake4.lua
@@ -5,25 +5,25 @@ project "window_glfw"
     files { "src/**.*" }
 	
 	
-    includedirs { "../../../include", "src", "../../../vendor/glfw/undicht" }
+    includedirs { "../../../include", "src", "../../../vendor/glfw/include", "../../../vendor/EGL-Registry/api"}
 	
 	configuration "debug - linux"
-		libdirs { "../../../vendor/glfw/undicht/GLFW/linux" }
+		libdirs {  "../../../vendor/glfw/build/src"}
 		links { "glfw3", "core", "pthread", "dl", "Xxf86vm", "Xrandr", "Xinerama", "Xcursor", "X11" }
 		flags { "Symbols" }
 		
 	configuration "release - linux"
-		libdirs { "../../../vendor/glfw/undicht/GLFW/linux" }
+		libdirs { "../../../vendor/glfw/undicht/GLFW/linux",  "../../../vendor/glfw/build/src" }
 		links { "glfw3", "core", "pthread", "dl", "Xxf86vm", "Xrandr", "Xinerama", "Xcursor", "X11" }
 		flags { "OptimizeSpeed" }
 		
 	configuration "debug - windows"
-		libdirs { "../../../vendor/glfw/undicht/GLFW/windows" }
+		libdirs { "../../../vendor/glfw/undicht/GLFW/windows",  "../../../vendor/glfw/build/src" }
 		links { "glfw3", "gdi32", "opengl32", "core" }
 		flags { "Symbols" }
 		
 	configuration "release - windows"
-		libdirs { "../../../vendor/glfw/undicht/GLFW/windows" }
+		libdirs { "../../../vendor/glfw/undicht/GLFW/windows",  "../../../vendor/glfw/build/src" }
 		links { "glfw3", "gdi32", "opengl32", "core" }
 		flags { "OptimizeSpeed"}
 
